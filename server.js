0f4f0086733f90c3048afe49d25d5be4109c10d2
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2021-01-06 00:45:50 +0100

    first commit and project set-up backend

--- server.js
+++ server.js
@@ -8,29 +8,7 @@ const path = require('path')
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Express API for JSONPlaceholder',
-    version: '1.0.0',
-    description:
-      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
-    license: {
-      name: 'Licensed Under MIT',
-      url: 'https://spdx.org/licenses/MIT.html',
-    },
-    contact: {
-      name: 'JSONPlaceholder',
-      url: 'https://jsonplaceholder.typicode.com',
-    },
-  },
-  servers: [
-    {
-      url: 'https://twitweeapp.herokuapp.com/',
-      description: 'Development server',
-    },
-  ],
-};
+
 
 const options = {
   swaggerDefinition,
